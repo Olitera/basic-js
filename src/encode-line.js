fec8e3021fe4f49b48ccd6d8aa0d9c4f55cfc331
Olitera <lu_gu@mail.ru>
2022-10-13 11:22:10 +0200

    feat: done encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -11,6 +11,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function encodeLine(str) {
+  let count = 1;
+  let readyString = '';
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] === str[i - 1]) {
+      count++
+    } else {
+      readyString += `${count===1 ? '' : count}${str[i - 1]}`
+      count = 1;
+    }
+    if (i === str.length - 1) {
+      readyString += `${count===1 ? '' : count}${str[i]}`
+    }
+  }
+  return readyString
   // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
