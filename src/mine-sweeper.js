e0bc1e5d393607c9303189228374e2cf7868bf10
Olitera <lu_gu@mail.ru>
2023-04-14 20:02:33 +0200

    fead: make mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -24,6 +24,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]
  */
 function minesweeper(matrix) {
+  function checkEl(a, b) {
+    let startI = a > 0 ? a - 1 : 0;
+    let startJ = b > 0 ? b - 1: 0;
+    let finishI = a < matrix.length - 1 ? a + 1 : a; 
+    let finishJ = b < matrix[0].length - 1 ? b + 1 : b; 
+    let sum = 0;
+    for (let i = startI; i <= finishI; i++) {
+      for (let j = startJ ; j <= finishJ; j++) {
+        console.log(i,j);
+        if (matrix[i][j] === true && (i !== a || j !==b)) {
+          console.log(i,j);
+          sum++;
+        }
+      }
+    }
+    return sum
+  }
+  let arr = [];
+  for(let i = 0; i < matrix.length; i++) {
+    arr.push(new Array());
+
+    for (let j = 0; j < matrix[i].length; j++) {
+      arr[i].push(checkEl(i,j));
+    }
+  }
+  return arr
   // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
